da4b3d076ddb2b18249bc598499b1fcd855dffbd
Ajang Rahmat <41977490+ajangrahmat@users.noreply.github.com>
2024-12-31 15:57:57 +0700

    Add Files via Upload

---
+++ src/FoonteDuino.cpp
@@ -0,0 +1,70 @@
+#include "KelasRobotFonnte.h"
+
+// Constructor untuk inisialisasi token
+KelasRobotFonnte::KelasRobotFonnte(const char* token) {
+    this->apiToken = token;
+}
+
+// Fungsi untuk menghubungkan ke WiFi dengan parameter ssid dan password
+void KelasRobotFonnte::connectToWiFi(const char* ssid, const char* password) {
+    this->ssid = ssid;
+    this->password = password;
+
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi...");
+    }
+    Serial.println("Connected to WiFi");
+}
+
+// Fungsi untuk mengirim pesan melalui API Fonnte
+void KelasRobotFonnte::sendMessage(const String& target, const String& message) {
+    if (WiFi.status() == WL_CONNECTED) {
+#if defined(ESP8266)
+        std::unique_ptr<BearSSL::WiFiClientSecure> client(new BearSSL::WiFiClientSecure);
+        client->setInsecure();  // Disable certificate validation for ESP8266
+        HTTPClient http;
+        http.begin(*client, serverName);
+#elif defined(ESP32)
+        HTTPClient http;
+        http.begin(serverName);
+#endif
+        http.addHeader("Authorization", this->apiToken);
+        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+
+        // Data POST yang dikirim ke server
+        String postData = "target=" + target + "&message=" + message + "&countryCode=" + countryCode;
+
+        // Kirim POST request
+        int httpResponseCode = http.POST(postData);
+
+        // Cek response dari server
+        if (httpResponseCode > 0) {
+            String response = http.getString();
+            Serial.println("HTTP Response code: " + String(httpResponseCode));
+            Serial.println("Response: " + response);
+        } else {
+            Serial.println("Error on sending POST: " + String(httpResponseCode));
+        }
+        http.end();
+    } else {
+        Serial.println("Error in WiFi connection");
+    }
+}
+
+// Fungsi untuk mengirim pesan dengan delay
+void KelasRobotFonnte::sendMessageWithDelay(const String& target, const String& message, unsigned long delayTime) {
+    delay(delayTime);  // Penundaan dalam milidetik
+    sendMessage(target, message);  // Panggil fungsi sendMessage setelah delay
+}
+
+// Fungsi untuk mengubah kode negara
+void KelasRobotFonnte::setCountryCode(const String& newCountryCode) {
+    this->countryCode = newCountryCode;
+}
+
+// Fungsi untuk mendapatkan country code saat ini
+String KelasRobotFonnte::getCountryCode() const {
+    return this->countryCode;
+}
---
+++ src/FoonteDuino.h
@@ -0,0 +1,41 @@
+#ifndef FOONTEDUINO_H
+#define FOONTEDUINO_H
+
+#if defined(ESP8266)
+  #include <ESP8266WiFi.h>
+  #include <ESP8266HTTPClient.h>
+  #include <WiFiClientSecureBearSSL.h>
+#elif defined(ESP32)
+  #include <WiFi.h>
+  #include <HTTPClient.h>
+#endif
+
+class FoonteDuino {
+  private:
+    String apiToken;
+    const char* ssid;
+    const char* password;
+    const char* serverName = "https://api.fonnte.com/send"; // API URL
+    String countryCode = "62"; // Default country code for Indonesia
+
+  public:
+    // Constructor untuk inisialisasi token
+    FoonteDuino(const char* token);
+
+    // Fungsi untuk menghubungkan ke WiFi dengan parameter ssid dan password
+    void connectToWiFi(const char* ssid, const char* password);
+
+    // Fungsi untuk mengirim pesan melalui API Fonnte
+    void sendMessage(const String& target, const String& message);
+
+    // Fungsi untuk mengirim pesan dengan delay
+    void sendMessageWithDelay(const String& target, const String& message, unsigned long delayTime);
+
+    // Fungsi untuk mengubah kode negara
+    void setCountryCode(const String& newCountryCode);
+
+    // Fungsi untuk mendapatkan country code saat ini
+    String getCountryCode() const;
+};
+
+#endif
